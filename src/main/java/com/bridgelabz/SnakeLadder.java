ff044053c74487bd3b558e2c82f6a806a100b5a4
ToshitaC <toshitachaudhari17@gmail.com>
2021-08-27 09:57:18 +0530

    Added:Player at exact position 100

--- src/main/java/com/bridgelabz/SnakeLadder.java
+++ src/main/java/com/bridgelabz/SnakeLadder.java
@@ -3,22 +3,34 @@ package com.bridgelabz;
 public class SnakeLadder {
     public static void main(String[] args) {
         int position = 0;
-        while (position <= 100) {
-            int snake = (int) Math.floor(Math.random() * 10) % 2;
-            int check = (int) Math.floor(Math.random() * 10) % 6 + 1;
+        int count = 0;
+        while (position < 100) {
+            int snake = (int) Math.floor(Math.random() * 10) % 3;
+            int check = (int) Math.floor(Math.random() * 10) % 7;
+            System.out.println(check);
+
             if (snake == 1) {
                 position = position + check;
-            } else if (snake == 1) {
+
+
+            } else if (snake == 2) {
                 position = position - check;
                 if (position < 0) {
                     position = 0;
                 }
             } else {
-                position = position;
+                count=count-1;
+            }
+
+            System.out.println("current postion  is: " + position);
+            if (position > 100) {
+                position = position - check;
+
+                System.out.println(position);
 
             }
-            System.out.println("current postion  is: " + position);
         }
+        System.out.println("win");
     }
 }
 
