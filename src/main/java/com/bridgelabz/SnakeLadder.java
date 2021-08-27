a7c355baea98d6fbc222401286af0e58d1a8a40a
ToshitaC <toshitachaudhari17@gmail.com>
2021-08-27 09:39:11 +0530

    Added:Roll the dice 1 to 6

--- src/main/java/com/bridgelabz/SnakeLadder.java
+++ src/main/java/com/bridgelabz/SnakeLadder.java
@@ -2,9 +2,7 @@ package com.bridgelabz;
 
 public class SnakeLadder {
     public static void main(String[] args) {
-        int position = 0;
-        int check = (int) Math.floor(Math.random() * 10) % 6 + 1;
-        position = position + check;
-        System.out.println("Current position is :" + position);
+        int check =(int) Math.floor(Math.random() * 10) %6 +1;
+        System.out.println("Current position is :" + check);
     }
 }
