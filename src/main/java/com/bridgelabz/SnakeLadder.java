eb080e46eb3c7486d147dd0e754b248795b640cc
ToshitaC <toshitachaudhari17@gmail.com>
2021-08-27 09:45:24 +0530

    Added:Move position for no play,snake,ladder

--- src/main/java/com/bridgelabz/SnakeLadder.java
+++ src/main/java/com/bridgelabz/SnakeLadder.java
@@ -2,7 +2,17 @@ package com.bridgelabz;
 
 public class SnakeLadder {
     public static void main(String[] args) {
-        int check =(int) Math.floor(Math.random() * 10) %6 +1;
-        System.out.println("Current position is :" + check);
+        int position = 46;
+        int snake = (int) Math.floor(Math.random() * 10) % 3;
+        int check = (int) Math.floor(Math.random() * 10) % 6 + 1;
+        if (snake == 2) {
+            position = position + check;
+        } else if (snake == 1) {
+            position = position - check;
+        } else {
+            position = position;
+
+        }
+        System.out.println("current position after 43 is: " + position);
     }
 }
