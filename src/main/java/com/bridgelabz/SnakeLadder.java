07db5af85c74e1cea9c9c4111440e443057d03ec
ToshitaC <toshitachaudhari17@gmail.com>
2021-08-27 10:03:12 +0530

    Added:Two player play the game

--- src/main/java/com/bridgelabz/SnakeLadder.java
+++ src/main/java/com/bridgelabz/SnakeLadder.java
@@ -4,33 +4,29 @@ public class SnakeLadder {
     public static void main(String[] args) {
         int position = 0;
         int count = 0;
-        while (position < 100) {
+        while (position <= 100) {
+            count++;
             int snake = (int) Math.floor(Math.random() * 10) % 3;
             int check = (int) Math.floor(Math.random() * 10) % 7;
-            System.out.println(check);
-
             if (snake == 1) {
                 position = position + check;
-
-
             } else if (snake == 2) {
                 position = position - check;
                 if (position < 0) {
                     position = 0;
                 }
             } else {
-                count=count-1;
+                count = count - 1;
             }
+            System.out.println("No of times dice roll is : " + count);
 
             System.out.println("current postion  is: " + position);
             if (position > 100) {
                 position = position - check;
 
-                System.out.println(position);
-
             }
+
         }
-        System.out.println("win");
     }
 }
 
